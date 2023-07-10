65fde59bd45ff0f0b985ed9790749cbb6216e9d8
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-07-10 22:49:19 +0300

    initial commit

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NextResponse} from 'next/server';
+
+type Feedback = {
+    name?: string,
+    email?: string,
+    message?: string
+}
+
+export async function POST(request: Request) {
+
+    const data: Feedback = await request.json()
+    console.log('data: ', data)
+
+    const {name, email, message } = data
+
+    return NextResponse.json({name, email, message})
+    
+}
---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main className='bg-slate-700 text-center font-bold'>Thank you</main>
+  )
+}
